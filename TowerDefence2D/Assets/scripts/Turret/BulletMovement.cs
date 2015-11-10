703c94977d03f4e37e6659db492bb16e31a6955a
18839 <18839@ma-web.nl>
2015-11-10 22:57:11 +0100

    Git Update 10-11-2015

---
+++ TowerDefence2D/Assets/scripts/Turret/BulletMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+    private float bulletVelocity;
+
+
+    private GameObject enemyLoc;
+    private Vector2 enemyVec;
+
+    void Start()
+    {
+        enemyLoc = GameObject.FindGameObjectWithTag("Enemy");
+        enemyVec = new Vector2(enemyLoc.transform.position.x, enemyLoc.transform.position.y);
+    }
+    
+    void Update()
+    {
+        Vector2.MoveTowards(new Vector2(0, 0), enemyVec, 5f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            // Do Something!
+        }
+    }
+}
