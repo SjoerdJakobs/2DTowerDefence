611662c7e5e6238d659bf64666208b4f5893588b
18839 <18839@ma-web.nl>
2015-11-13 23:20:59 +0100

    Git Update 13-11-2015

--- TowerDefence2D/Assets/scripts/Turret/BulletMovement.cs
+++ TowerDefence2D/Assets/scripts/Turret/BulletMovement.cs
@@ -3,28 +3,43 @@ using System.Collections;
 
 public class BulletMovement : MonoBehaviour {
 
+    //Floats
     private float bulletVelocity;
+    //Floats
 
-
+    //GameObjects
+    private GameObject thisLoc;
     private GameObject enemyLoc;
+    //GameObjects
+
+    //Vector2
     private Vector2 enemyVec;
+    private Vector2 thisVec;
+    //Vector2
 
     void Start()
     {
+        
+        bulletVelocity = 70f;
+
         enemyLoc = GameObject.FindGameObjectWithTag("Enemy");
-        enemyVec = new Vector2(enemyLoc.transform.position.x, enemyLoc.transform.position.y);
+        thisLoc = GameObject.Find("BulletSpawner");
+        thisVec = new Vector2(enemyLoc.transform.position.x, enemyLoc.transform.position.y);
+        enemyVec = new Vector2(thisLoc.transform.position.x, thisLoc.transform.position.y);
     }
     
     void Update()
     {
-        Vector2.MoveTowards(new Vector2(0, 0), enemyVec, 5f);
+       
+        //Vector2.MoveTowards(thisVec, enemyVec, bulletVelocity);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Weapon")
         {
-            // Do Something!
+            Debug.Log("Test");
+            this.gameObject.GetComponent<Rigidbody2D>().AddForce((enemyLoc.transform.position - transform.position) * bulletVelocity * Time.deltaTime, ForceMode2D.Impulse);
         }
     }
 }
