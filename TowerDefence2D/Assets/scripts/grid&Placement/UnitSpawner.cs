28016152981b682af80cac516efca0f6361cbfcd
SjoerdJakobs <sjakobs@chello.nl>
2015-11-16 13:29:45 +0100

    birthday bugs 16-11

---
+++ TowerDefence2D/Assets/scripts/grid&Placement/UnitSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private Transform enemy1;
+    [SerializeField]
+    private float waitTime;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawnRepeater", 0, waitTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    void spawnRepeater()
+    {
+        Instantiate(enemy1, new Vector2(-40, 0), Quaternion.identity);
+    }
+}
